7b898e461d1372d1a12a050f0365dd7c741c7de4
OtterB8 <36349999+OtterB8@users.noreply.github.com>
2019-04-26 17:01:26 +0700

    Update TimeLibraryWithMenu.asm

--- TimeLibraryWithMenu.asm
+++ TimeLibraryWithMenu.asm
@@ -246,7 +246,7 @@ Option4:
 	add $t1, $zero, $v0	# $t1 = year
 	
 	addi $v0, $zero, 1	# syscall print integer
-	add  $a0, $zero, $t1	# $a0 = isLeapYear value
+	add  $a0, $zero, $t1	# $a0 = year
 	syscall
 	
 	beq $t0, $zero, NotLeapYear
@@ -313,14 +313,41 @@ Option5:
 	
 Option6:
 	#Save registers values
-	addi $sp, $sp, -4
+	addi $sp, $sp, -20
+	sw $v1, 16($sp)
+	sw $v0, 12($sp)
+	sw $a0, 8($sp)
+	sw $t0, 4($sp)
 	sw $ra, 0($sp)
 	#----------------
+	la $a0, time
+	jal LeapYear_Nearest
 	
+	add $t0, $zero, $v0	# keep first nearest leap year
+	
+	addi $v0, $zero, 4	# syscall print string
+	la   $a0, result
+	syscall
+	
+	addi $v0, $zero, 1	# syscall print integer
+	add  $a0, $zero, $t0	# $a0 = first nearest leap year
+	syscall
+	
+	addi $v0, $zero, 4	# syscall print string
+	la   $a0, Comma_Str
+	syscall
+	
+	addi $v0, $zero, 1	# syscall print integer
+	add  $a0, $zero, $v1	# $a0 = second nearest leap year
+	syscall
 	endOption6:
 		#Restore registers values
 		lw $ra, 0($sp)
-		addi $sp, $sp, 4
+		lw $t0, 4($sp)
+		lw $a0, 8($sp)
+		lw $v0, 12($sp)
+		lw $v1, 16($sp)
+		addi $sp, $sp, 20
 		#----------------
 		jr $ra
 	
@@ -1430,3 +1457,77 @@ WeekDay: # WeekDay(char* a0)
  		  addi $sp, $sp, 4
 		jr $ra
 #End of WeekDay function
+
+LeapYear_Nearest: # int* LeaYearNearest(char* time)
+	addi $sp, $sp, -4
+	  sw $ra, 0($sp) # Luu $ra vao Stack
+	addi $sp, $sp, -4
+	  sw $a0, 0($sp) # Luu $a0 vao Stack
+	
+	jal Year
+	
+	add $t0, $v0, $zero # t0 = v0 : year
+	add $t1, $v0, $zero # t1 = v0 : year
+		
+	LeapYear_Nearest_Loop_1: # Tim nam nhuan nho hon 
+		addi $t0, $t0, -1 # t0--
+		add $a0, $t0, $zero # a0 = t0
+		jal LeapYear_2	
+		bnez $v0, LeapYear_Nearest_Loop_2 # if v0 = 1 then quit loop_1
+		j LeapYear_Nearest_Loop_1
+	
+	LeapYear_Nearest_Loop_2: # Tim nam nhuan lon hon
+		addi $t1, $t1, 1 # t1++
+		add $a0, $t1, $zero # a0 = t1
+		jal LeapYear_2	
+		bnez $v0, LeapYear_Nearest_Loop_Exit # if v0 = 1 then quit loop_2
+		j LeapYear_Nearest_Loop_2
+	
+	LeapYear_Nearest_Loop_Exit:
+		add $v0, $t0, $zero # v0 chua nam nho hon
+ 		add $v1, $t1, $zero # v1 chua nam lon hon
+
+	LeapYear_Nearest_Exit:
+		lw $a0, 0($sp)
+		  addi $sp, $sp, 4 # Tra ve gia tri ban dau cua $a0
+		lw $ra, 0($sp)
+		  addi $sp, $sp, 4 # Tra ve gia tri ban dau cua $ra
+		jr $ra
+		
+
+LeapYear_2: # bool LeapYear(int a0)
+	addi $sp, $sp, -4
+	  sw $ra, 0($sp) # Luu $ra vao Stack
+	addi $sp, $sp, -4
+	  sw $t0, 0($sp) # Luu $t0 vao Stack	
+	addi $sp, $sp, -4
+	  sw $t1, 0($sp) # Luu $t1 vao Stack
+
+	add $t0, $a0, $zero # t0 = a0, t0 la nam
+	add $v0, $zero, $zero # v0 = 0
+	div $t1, $t0, 400 # t0 / 400
+	mfhi $t1 # t1 = t0 % 400
+
+	beqz $t1, LeapYear_2_IF # If t1 = 0 then v0 = 1
+	beqz $zero, LeapYear_2_IF_Exit
+
+	LeapYear_2_IF: # Kiem tra dieu kien a0 chia het cho 400
+		addi $v0, $zero, 1 # v0 = 1
+		j LeapYear_2_Exit
+
+	LeapYear_2_IF_Exit:
+		div $t1, $t0, 4 # t0 / 4
+		mfhi $t1 # t1 = t0 % 4
+		div $t2, $t0, 100
+		mfhi $t2 # t1 = t0 $ 100
+		add $t1, $t1, $t2 # t1 = t1 + t2
+		beq $t1, $t2, LeapYear_2_IF # If t1 = t1 + t2 then v0 = 1 
+
+	LeapYear_2_Exit:
+		lw $t1, 0($sp) # Lay gia tri cua $t1 trong Stack
+		  addi $sp, $sp, 4
+		lw $t0, 0($sp) # Lay gia tri cua $t0 trong Stack
+		  addi $sp, $sp, 4
+		lw $ra, 0($sp) # Lay gia tri cua $ra trong Stack
+		  addi $sp, $sp, 4
+		jr $ra
